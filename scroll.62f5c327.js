597bdd6a428b859821c28f4aadeeefd683410310
Marina Vyshnevska <Marinessa393@users.noreply.github.com>
2020-12-08 16:03:39 +0200

    Updates

--- scroll.62f5c327.js
+++ scroll.62f5c327.js
@@ -178,7 +178,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52731" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54841" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
